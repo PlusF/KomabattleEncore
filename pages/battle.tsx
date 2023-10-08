5b42f152ed720d66146040c244b3e085455efa11
PlusF <rkaneda896@gmail.com>
2023-10-08 21:08:53 +0900

    implement basic tournament style

--- pages/battle.tsx
+++ pages/battle.tsx
@@ -4,12 +4,82 @@ import { getContents } from '../lib/spreadsheet';
 import { Dancer } from '../lib/dancer';
 import { Heading, Text } from '@chakra-ui/react';
 
+const TournamentBracket = () => {
+    return (
+        <article id='tournament-container'>
+
+            <section className='tournament-round'>
+                <div className='player-box'>Player 1</div>
+                <div className='player-box'>Player 1</div>
+                <div className='player-box'>Player 1</div>
+                <div className='player-box'>Player 1</div>
+                <div className='player-box'>Player 1</div>
+                <div className='player-box'>Player 1</div>
+                <div className='player-box'>Player 1</div>
+                <div className='player-box'>Player 1</div>
+            </section>
+            
+            <div className="connecter">
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+            
+            <div className="line">
+                <div>
+                </div><div>
+                </div><div>
+                </div><div>
+                </div>
+            </div>
+            
+            <section className='tournament-round' id="quarterFinals">
+                <div className='player-box'>Player 1</div>
+                <div className='player-box'>Player 1</div>
+                <div className='player-box'>Player 1</div>
+                <div className='player-box'>Player 1</div>
+            </section>
+            
+            <div className="connecter" id="conn2">
+                <div></div>
+                <div></div>
+            </div>
+            
+            <div className="line" id="line2">
+                <div></div>
+                <div></div>
+            </div>
+            
+            <section className='tournament-round' id="semiFinals">
+                <div className='player-box'>Player 1</div>
+                <div className='player-box'>Player 1</div>
+            </section>
+            
+            <div className="connecter" id="conn3">
+                <div></div>
+            </div>
+            
+            <div className="line" id="line3">
+                <div></div>
+            </div>
+            
+            <section className='tournament-round' id="final">
+                <div className='player-box'>Player 1</div>
+            </section>
+        
+        </article>
+    );
+};
+
+
 export default function Battle({ dancers }: { dancers: Dancer[] }) {
     return (
         <>
         <Heading>Battle</Heading>
         <Text>Coming soon...</Text>
         <Link href="/">&larr; Go Back</Link>
+        <TournamentBracket />
         </>
     )
 }
