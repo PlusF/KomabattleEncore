6b0b7e54a3d48ace4f87be23e8eae21d7affbcac
PlusF <rkaneda896@gmail.com>
2023-12-16 16:55:08 +0900

    update detail

--- components/detail.tsx
+++ components/detail.tsx
@@ -13,10 +13,10 @@ function Detail() {
     return (
         <VStack sx={vstackStyle}>
             <Text>
-                12/16 16:30 ~ 20:30 (暫定)
+                12/16 16:30 ~ 21:30
             </Text>
             <Text>
-                ノア学芸大学スタジオ
+                ノア学芸大学スタジオ 7st
             </Text>
             <Text>
                 1on1 フルトーナメント
